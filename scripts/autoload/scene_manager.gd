449956b91638394f458ddff57ee54a81235e3ef0
Jules T460 <jules.pesin@gmail.com>
2024-02-16 21:26:09 +0100

    camera + bg

---
+++ scripts/autoload/scene_manager.gd
@@ -0,0 +1,13 @@
+extends Node
+
+var root
+var main : Node
+var game : Node :
+	get : return main.get_node("Game")
+	
+var web : bool = false
+
+func _ready() -> void:
+	if(OS.get_distribution_name() == ""): web = true
+	root = get_tree().root
+	main = root.get_node("Main")
