b0ddb3ef60180d189021e28f5c4c027cf2cf9fe1
lopotichaaaa <ninokeravel@gmail.com>
2024-02-17 17:47:23 +0100

    score system

--- scripts/game_manager.gd
+++ scripts/game_manager.gd
@@ -9,12 +9,16 @@ class_name GameManager extends Node
 @onready var clock : Clock = $hands/RightArm/Hand/HandSprite/Clock
 @onready var eyes : Eyes = $Transitions/Eyes
 @onready var bite : Bite = $Transitions/Bite
+@onready var score : int = 0
 
 func _ready():
 	round_manager.new_round()
 	
 func loose_condition() -> bool:
 	return sanity_manager.sanity < sanity_manager.min_sanity
+
+func gain_score(points:int):
+	score+=points
 
 func process_round():
 	hands_manager.activate(false)
@@ -26,6 +30,7 @@ func process_round():
 	await eyes.open()
 	await clock.run_clock(round_manager.recognition_duration)
 	await stuff_manager.check_stuff_list_positions()
+	print(score)
 	await TimeManager.sleep(1)
 	
 	if loose_condition():
@@ -37,4 +42,5 @@ func process_round():
 func loose():
 	print("You loose")
 	SceneManager.load_score()
+	SceneManager.score.set_score(score)
 	
