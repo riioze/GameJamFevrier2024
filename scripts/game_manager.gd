574d36f3eb7b3c238c4b77f5338c209ecfe39556
Jules T460 <jules.pesin@gmail.com>
2024-02-17 15:27:40 +0100

    sprites and watch

--- scripts/game_manager.gd
+++ scripts/game_manager.gd
@@ -5,7 +5,8 @@ class_name GameManager extends Node
 @onready var eyes : Eyes = $Eyes
 @onready var round_manager : RoundManager = $rounds
 @onready var score_manager : ScoreManager = SceneManager.score
-@onready var clock : Clock = $hands/LeftArm/Hand/HandSprite/Clock
+@onready var clock : Clock = $hands/RightArm/Hand/HandSprite/Clock
+@onready var hands_manager : HandsManager = $hands
 
 #var recognition_phase_duration: float = 3
 #var ordering_phase_duration: float = 3
@@ -18,9 +19,12 @@ func loose_condition() -> bool:
 	return sanity_manager.sanity < sanity_manager.min_sanity
 
 func process_round():
+	hands_manager.activate(false)
 	await TimeManager.sleep(round_manager.recognition_duration)
 	await eyes.close()
 	stuff_manager.scramble_stuff(round_manager.moving_stuff_count)
+	clock.reset()
+	hands_manager.activate(true)
 	await eyes.open()
 	await clock.run_clock(round_manager.recognition_duration)
 	stuff_manager.check_stuff_list_positions()
